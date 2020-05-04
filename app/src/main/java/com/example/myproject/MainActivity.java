a759352e4f096c4bb1a543ebaf725a5ac765712b
idogolan12 <idogolan12312@gmail.com>
2020-05-05 00:40:44 +0300

    1

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -3,24 +3,99 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    SQLiteDatabase sqdb;
+    DBHelper my_db;
+    Button btnGolog;
+    EditText etNickLog, etPassLog;
+    String[] infa=new String[2];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        my_db=new DBHelper(this);
+        sqdb=my_db.getWritableDatabase();
+        sqdb.close();
+
+        btnGolog= (Button) findViewById(R.id.btnGolog);
+        btnGolog.setOnClickListener(this);
+        etNickLog= (EditText) findViewById(R.id.etNickLog);
+        etPassLog= (EditText) findViewById(R.id.etPassLog);
     }
 
-    public void regist(View view) {
+    public void reg(View view) {
         Intent go = new Intent(this,register.class);
         startActivity(go);
     }
 
-    public void log(View view) {
-        Intent go = new Intent(this,home.class);
-        startActivity(go);
+    @Override
+    public void onClick(View view) {
+        infa[0] = etNickLog.getText().toString();
+        infa[1] = etPassLog.getText().toString();
+        etNickLog.setText("");
+        etPassLog.setText("");
+        if (is_empty()) {
+            Intent goReg=new Intent(this, register.class);
+            startActivity(goReg);
+            return;
+        }
+        if (is_found(infa[0], infa[1])) {
+            Toast.makeText(this,
+                    "Ok",
+                    Toast.LENGTH_LONG).show();
+            Intent goStart=new Intent(this, home.class);
+            startActivity(goStart);
+        }
+        else {
+            Intent goReg=new Intent(this, register.class);
+            startActivity(goReg);
+        }
     }
+
+    private boolean is_empty() {
+        sqdb=my_db.getWritableDatabase();
+        int count=0;
+        Cursor c=sqdb.query(DBHelper.TABLE_NAME,
+                null, null, null, null, null, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()&&count==0) {
+            count++;
+            c.moveToNext();
+        }
+        c.close();
+        return (count==0);
+    }
+
+    private boolean is_found(String s1, String s2) {
+        boolean flag=false;
+        sqdb=my_db.getWritableDatabase();
+        Cursor c=sqdb.query(DBHelper.TABLE_NAME,
+                null, null, null, null, null, null);
+        int col1=c.getColumnIndex(DBHelper.NICKNAME);
+        int col2=c.getColumnIndex(DBHelper.PASS);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            String t1=c.getString(col1);
+            String t2=c.getString(col2);
+            if (s1.equals(t1)&&s2.equals(t2))
+                flag=true;
+            c.moveToNext();
+        }
+        c.close();
+        sqdb.close();
+        return flag;
+    }
+
+
 }
+
--- app/src/main/java/com/example/myproject/register.java
+++ app/src/main/java/com/example/myproject/register.java
@@ -2,20 +2,112 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class register extends AppCompatActivity {
+public class register extends AppCompatActivity implements View.OnClickListener{
 
+    SQLiteDatabase sqdb;
+    DBHelper my_db;
+    Button bt3;
+    EditText etnick,etpass,etmail,etphone;
+    String[] infa = new String[4];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        my_db = new DBHelper(this);
+        sqdb = my_db.getWritableDatabase();
+        sqdb.close();
+
+        bt3 = findViewById(R.id.bt3);
+        bt3.setOnClickListener(this);
+        etnick = findViewById(R.id.etnick);
+        etpass = findViewById(R.id.etpass);
+        etmail = findViewById(R.id.etmail);
+        etphone = findViewById(R.id.etphone);
     }
 
-    public void tohome(View view) {
-        Intent go = new Intent(this,home.class);
-        startActivity(go);
+
+    @Override
+    public void onClick(View view) {
+        infa[0] = etnick.getText().toString();
+        infa[1] = etpass.getText().toString();
+        infa[2] = etmail.getText().toString();
+        infa[3] = etphone.getText().toString();
+        etnick.setText("");
+        etpass.setText("");
+        etmail.setText("");
+        etphone.setText("");
+
+
+        if (is_empty()) { //naor: why not to chek that in the is_found function?
+            go_regist();
+            return;
+        }
+        if (is_found(infa[0], infa[1])) {
+            Toast.makeText(this,
+                    "This name and pass is found",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        } else
+            go_regist();
     }
+
+    private boolean is_found(String s1, String s2) {
+        boolean flag=false;
+        sqdb=my_db.getWritableDatabase();
+        Cursor c=sqdb.query(DBHelper.TABLE_NAME,
+                null, null, null, null, null, null);
+        int col1=c.getColumnIndex(DBHelper.NICKNAME);
+        int col2=c.getColumnIndex(DBHelper.PASS);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            String t1=c.getString(col1);
+            String t2=c.getString(col2);
+            if (s1.equals(t1)&&s2.equals(t2))
+                flag=true;
+            c.moveToNext();
+        }
+        c.close();
+        sqdb.close();
+        return flag;
+    }
+
+    private boolean is_empty() {
+        sqdb=my_db.getWritableDatabase();
+        int count=0;
+        Cursor c=sqdb.query(DBHelper.TABLE_NAME,
+                null, null, null, null, null, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()&&count==0) {
+            count++;
+            c.moveToNext();
+        }
+        c.close();
+        return (count==0);
+    }
+
+    private void go_regist() {
+        ContentValues cv=new ContentValues();
+        cv.put(my_db.NICKNAME, infa[0]);
+        cv.put(my_db.PASS, infa[1]);
+        cv.put(my_db.EMAIL, infa[2]);
+        cv.put(my_db.PHONE, infa[3]);
+
+        sqdb=my_db.getWritableDatabase();
+        sqdb.insert(my_db.TABLE_NAME, null, cv);
+        sqdb.close();
+        Intent goStart=new Intent(this, home.class);
+        startActivity(goStart);
+    }
+
 }
+
