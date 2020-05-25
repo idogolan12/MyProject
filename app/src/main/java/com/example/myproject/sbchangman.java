c731a0fe6614ae2874d536e8713f350b5f51d207
idogolan12 <idogolan12312@gmail.com>
2020-05-25 17:04:58 +0300

    12

--- app/src/main/java/com/example/myproject/sbchangman.java
+++ app/src/main/java/com/example/myproject/sbchangman.java
@@ -1,5 +1,6 @@
 package com.example.myproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
@@ -7,10 +8,14 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class sbchangman extends AppCompatActivity {
+    Intent go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +23,46 @@ public class sbchangman extends AppCompatActivity {
         setContentView(R.layout.activity_sbchangman);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.credit:
+                 go = new Intent(this,credit.class);
+                 startActivity(go);
+            case R.id.guide:
+                 go = new Intent(this,guide.class);
+                startActivity(go);
+            case R.id.achievements:
+                go = new Intent(this,achievements.class);
+                startActivity(go);
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
     public void ams(View view) {
-        Intent go = new Intent(this,hangm.class);
+        go = new Intent(this,hangm.class);
         go.putExtra("wordesType","a");
         startActivity(go);
     }
 
     public void cny(View view) {
-        Intent go = new Intent(this,hangm.class);
+        go = new Intent(this,hangm.class);
         go.putExtra("wordesType","b");
         startActivity(go);
     }
 
     public void cts(View view) {
-        Intent go = new Intent(this,hangm.class);
+        go = new Intent(this,hangm.class);
         go.putExtra("wordesType","c");
         startActivity(go);
     }
