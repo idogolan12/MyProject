4a3715ed423b4c93d60ea3791dc7ef5526340190
idogolan12 <idogolan12312@gmail.com>
2020-06-09 01:14:44 +0300

    1

--- app/src/main/java/com/example/myproject/themouseandthechocolate.java
+++ app/src/main/java/com/example/myproject/themouseandthechocolate.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class themouseandthechocolate extends AppCompatActivity {
     EditText etLine, etRaw;
-
+    String[] infa2 = new String[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,16 @@ public class themouseandthechocolate extends AppCompatActivity {
         setContentView(R.layout.activity_themouseandthechocolate);
         etLine = findViewById(R.id.edLine);
         etRaw = findViewById(R.id.edRaw);
+        infa2[0] = etLine.getText().toString();
+        infa2[1] = etRaw.getText().toString();
+
     }
 
     public void goToGame(View view) {
-        Intent intent = new Intent(this ,TheMouseAndTheChocolateGame.class);
-        intent.putExtra("line", etLine.getText().toString());
-        intent.putExtra("raw", etRaw.getText().toString());
-        startActivity(intent);
+            Intent intent = new Intent(this, TheMouseAndTheChocolateGame.class);
+            intent.putExtra("line", etLine.getText().toString());
+            intent.putExtra("raw", etRaw.getText().toString());
+            startActivity(intent);
+
     }
 }
